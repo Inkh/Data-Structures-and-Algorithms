1d929188dd4d2c34ce8f77e662530a86b5965a4e
inkh <jhwe92@gmail.com>
2018-10-10 11:35:11 -0700

    Added comments to solution for clarity

--- Challenges/ArrayShift/ArrayShift/Program.cs
+++ Challenges/ArrayShift/ArrayShift/Program.cs
@@ -14,21 +14,26 @@ namespace ArrayShift
         static int[] ArrayShift(int[] arr, int val)
         {
             int[] newArr = new int[arr.Length + 1];
+            //Loop through new array length (arr.Length + 1)
             for (int i = 0; i < newArr.Length; i++)
             {
+                //Cast integer to double, then round it up.
                 if (i < Math.Ceiling((double)arr.Length / 2))
                 {
                     newArr[i] = arr[i];
                 }
+                //Once we're at middle point, set index value to input value
                 else if (i == Math.Ceiling((double)arr.Length /2 ))
                 {
                     newArr[i] = val;
                 }
+                //Past middle point. Our index is 1 ahead of original array index. Therefore we set new arr index value as i - 1.
                 else
                 {
                     newArr[i] = arr[i - 1];
                 }
             }
+            //Console log to make sure we get correct output.
             Console.WriteLine(string.Join(",", newArr));
             return newArr;
         }
