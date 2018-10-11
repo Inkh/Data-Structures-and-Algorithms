f93f4ecc34d8741c414e7d3aa7b255e3213e734c
inkh <jhwe92@gmail.com>
2018-10-10 21:59:09 -0700

    Set up sln. Implemented binary search

---
+++ Challenges/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sample input. Expected output to be : 6
+            int num = 150;
+            int[] sortedArr = { 1, 3, 5, 6, 8, 10, 150, 320 };
+            Console.WriteLine(BinarySearch(sortedArr, num));
+
+            //Sample Input 2. Expected output to be : -1
+            int[] emptyArr = { };
+            Console.WriteLine(BinarySearch(emptyArr, num));
+        }
+
+        static int BinarySearch(int[] arr, int val)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+            int mid;
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                if (arr[mid] == val)
+                {
+                    return mid;
+                }
+                else if (arr[mid] < val)
+                {
+                    start = mid + 1;
+                }
+                else if (arr[mid] > val)
+                {
+                    end = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
