5120327ac541b465b1eb1d42faa4397f44893e39
inkh <jhwe92@gmail.com>
2018-10-18 11:46:43 -0700

    finished writing methods

--- Challenges/ll_merge/ll_merge/Program.cs
+++ Challenges/ll_merge/ll_merge/Program.cs
@@ -18,14 +18,35 @@ namespace ll_merge
 
             LList merged = MergeLL(first, second);
 
+            Console.WriteLine("First method:");
             merged.Print();
+            Console.WriteLine("=");
+
+            LList mergeTwo = MergeTwo(first, second);
+
+            Console.WriteLine("Second Method");
+            mergeTwo.Print();
+            Console.WriteLine("=");
+
+
+            Console.WriteLine("Replacing one node");
+            FindAndReplaceNode(first, new Node(3));
+            Console.WriteLine("Updated list from method #2");
+            mergeTwo.Print();
         }
 
+        /// <summary>
+        /// Takes two linked lists and zips them up with alternating nodes.
+        /// Creates a new list with new nodes reflecting values.
+        /// </summary>
+        /// <param name="list1">First linked list</param>
+        /// <param name="list2">Second linked list</param>
+        /// <returns>Merged linked list</returns>
         static LList MergeLL(LList list1, LList list2)
         {
             Node curr1 = list1.Head;
             Node curr2 = list2.Head;
-            LList returnList = new LList(curr1);
+            LList returnList = new LList(new Node(curr1.Value));
 
             curr1 = curr1.Next;
 
@@ -50,5 +71,59 @@ namespace ll_merge
 
             return returnList;
         }
+
+        /// <summary>
+        /// Takes two linked lists and zips them up with alternating nodes.
+        /// However, this method does not create an entire new list.
+        /// </summary>
+        /// <param name="list1">First linked list</param>
+        /// <param name="list2">Second linked list</param>
+        /// <returns>Merged linked list</returns>
+        static LList MergeTwo(LList list1, LList list2)
+        {
+            Node curr1 = list1.Head;
+            Node curr2 = list2.Head;
+            LList returnList = new LList(curr1);
+            Node returnCurr = returnList.Head;
+            curr1 = curr1.Next;
+
+            while (curr1 != null || curr2 != null)
+            {
+                if (curr2 != null)
+                {
+                    returnCurr.Next = curr2;
+                    curr2 = curr2.Next;
+                    returnCurr = returnCurr.Next;
+                }
+                if (curr1 != null)
+                {
+                    returnCurr.Next = curr1;
+                    curr1 = curr1.Next;
+                    returnCurr = returnCurr.Next;
+                }
+            }
+            return returnList;
+        }
+
+        /// <summary>
+        /// Finds specific node that holds matching value and changes it to 100.
+        /// This method is a helper for testing.
+        /// </summary>
+        /// <param name="list">Linked List</param>
+        /// <param name="node">Node to match</param>
+        static void FindAndReplaceNode(LList list, Node node)
+        {
+            Node curr = list.Head;
+
+            while (curr.Next != null)
+            {
+                if (curr.Value.ToString() == node.Value.ToString())
+                {
+                    curr.Value = 100;
+                }
+                curr = curr.Next;
+            }
+        }
     }
+
 }
