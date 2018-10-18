13ad34c61550ca1d66a326c5b33e7191ec314471
inkh <jhwe92@gmail.com>
2018-10-18 11:48:06 -0700

    Added test file

---
+++ Challenges/ll_merge/MergeTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MergeTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void WillMergeOnEqualTest()
+        {
+
+        }
+    }
+}
