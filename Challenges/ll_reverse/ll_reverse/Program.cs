c7ea107c3eeca589a3a8fbee9ae50ee10df07232
inkh <jhwe92@gmail.com>
2018-10-21 17:07:46 -0700

    Completed reverse LL

--- Challenges/ll_reverse/ll_reverse/Program.cs
+++ Challenges/ll_reverse/ll_reverse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LinkedList.Classes;
 
 namespace ll_reverse
 {
@@ -7,6 +8,37 @@ namespace ll_reverse
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            LList myList = new LList(new Node(10));
+            myList.Append(new Node(20));
+            myList.Append(new Node(30));
+            myList.Append(new Node(40));
+            myList.Append(new Node(50));
+
+            myList.Print();
+
+            ReverseLL(myList);
+
+            myList.Print();
+        }
+
+        /// <summary>
+        /// Reverses a Linked List
+        /// </summary>
+        /// <param name="input">LinkedList input</param>
+        static void ReverseLL(LList input)
+        {
+            Node curr = input.Head;
+            Node prev = null;
+            
+            while (curr != null)
+            {
+                Node next = curr.Next;
+                curr.Next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            input.Head = prev;
         }
     }
 }
