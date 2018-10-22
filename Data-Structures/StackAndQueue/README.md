7fb26eec00845dd01cf76270f9f61d15ec39cb31
inkh <jhwe92@gmail.com>
2018-10-22 11:30:35 -0700

    complete README and assets plug

---
+++ Data-Structures/StackAndQueue/README.md
@@ -0,0 +1,44 @@
+# Stack And Queue
+
+Implements the Stack and Queue data structure. Stacks follow a FILO (First in last out) flow, while Queue follows a FIFO (First in first out) flow.
+
+## Space and Time Complexity
+
+Peek
+- Returns the first/top node of the data structure.
+
+> Time: O(1)
+
+> Space: O(1)
+
+Push
+- Pushes a new node to the top of the stack.
+
+> Time: O(1)
+
+> Space: O(1)
+
+Pop
+- Removes the node on the top of the stack.
+
+> Time: O(1)
+
+> Space: O(1)
+
+Enqueue
+- Adds a new node to the rear end of queue.
+
+> Time: O(1)
+
+> Space: O(1)
+
+Dequeue
+- Removes the Front node of a Queue.
+
+> Time: O(1)
+
+> Space: O(1)
+
+## Visual
+
+![stackandqueue](../../assets/stackqueue.PNG)
--- Data-Structures/StackAndQueue/StackAndQueue/Program.cs
+++ Data-Structures/StackAndQueue/StackAndQueue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using StackAndQueue.Classes;
 
 namespace StackAndQueue
 {
@@ -6,7 +7,29 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Stack flow");
+            Stack myStack = new Stack(new Node(10));
+            myStack.Push(new Node(20));
+            myStack.Push(new Node(30));
+
+            while (myStack.Top != null)
+            {
+                Console.Write(myStack.Peek().Value + "-->");
+                myStack.Top = myStack.Top.Next;
+            }
+            Console.WriteLine("NULL");
+
+            Console.WriteLine("Queue Flow");
+            Queue myQueue = new Queue(new Node(10));
+            myQueue.Enqueue(new Node(20));
+            myQueue.Enqueue(new Node(30));
+
+            while (myQueue.Front != null)
+            {
+                Console.Write(myQueue.Peek().Value + "-->");
+                myQueue.Front = myQueue.Front.Next;
+            }
+            Console.WriteLine("NULL");
         }
     }
 }
