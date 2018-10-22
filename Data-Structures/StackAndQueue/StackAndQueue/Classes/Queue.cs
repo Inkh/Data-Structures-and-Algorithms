995d7aadc9936065e223c7a92973f022180a7445
inkh <jhwe92@gmail.com>
2018-10-21 18:49:45 -0700

    Implemented stack, queue, and node classes

---
+++ Data-Structures/StackAndQueue/StackAndQueue/Classes/Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue.Classes
+{
+    class Queue
+    {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+        public Queue(Node node)
+        {
+            Front = node;
+            Rear = node;
+        }
+
+        /// <summary>
+        /// Adds a node to the rear end of queue
+        /// </summary>
+        /// <param name="node">Node to be added</param>
+        public void Enqueue(Node node)
+        {
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        /// <summary>
+        /// Removes a node from the front of queue
+        /// </summary>
+        /// <returns>Removed node</returns>
+        public Node Dequeue()
+        {
+            Node temp = Front;
+            Front = Front.Next;
+            temp.Next = null;
+            return temp;
+        }
+    }
+}
---
+++ Data-Structures/StackAndQueue/StackAndQueue/Classes/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue.Classes
+{
+    public class Stack
+    {
+        public Node Top { get; set; }
+
+        /// <summary>
+        /// 'Peeks' at the current node in stack.
+        /// </summary>
+        /// <returns></returns>
+        public Node Peek()
+        {
+            return Top;
+        }
+
+        /// <summary>
+        /// Removes the first node on the stack.
+        /// </summary>
+        /// <returns>Removed node</returns>
+        public Node Pop()
+        {
+            Node temp = Peek();
+            Top = Top.Next;
+            temp.Next = null;
+
+            return temp;
+        }
+
+        /// <summary>
+        /// Inserts input node onto the stack.
+        /// </summary>
+        /// <param name="node">Node to be added</param>
+        public void Push(Node node)
+        {
+            node.Next = Top;
+            Top = node;
+        }
+    }
+}
