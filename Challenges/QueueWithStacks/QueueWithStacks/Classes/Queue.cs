30bf8372daf046145b6f6d80b2eeadfead2bb615
inkh <jhwe92@gmail.com>
2018-10-22 20:01:41 -0700

    completed queue class with stacks

--- Challenges/QueueWithStacks/QueueWithStacks/Classes/Queue.cs
+++ Challenges/QueueWithStacks/QueueWithStacks/Classes/Queue.cs
@@ -15,5 +15,31 @@ namespace QueueWithStacks.Classes
             Front = front;
             Rear = rear;
         }
+
+        public void Enqueue(Node node)
+        {
+            Rear.Push(node);
+        }
+
+        public Node Dequeue()
+        {
+            int counter = 0;
+
+            while (counter < Rear.Size)
+            {
+                Front.Push(Rear.Pop());
+                counter++;
+            }
+            Node returnNode = Front.Pop();
+            counter = 0;
+
+            while (counter < Front.Size)
+            {
+                Rear.Push(Front.Pop());
+                counter++;
+            }
+
+            return returnNode;
+        }
     }
 }
