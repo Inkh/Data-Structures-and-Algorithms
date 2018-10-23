96c60c36395e9335cd77c314b4d7bf72cdc1f595
inkh <jhwe92@gmail.com>
2018-10-22 19:54:17 -0700

    Init. Created queue class

---
+++ Challenges/QueueWithStacks/QueueWithStacks/Classes/Queue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackAndQueue.Classes;
+
+namespace QueueWithStacks.Classes
+{
+    public class Queue
+    {
+        public Stack Front { get; set; }
+        public Stack Rear { get; set; }
+
+        public Queue(Stack front, Stack rear)
+        {
+            Front = front;
+            Rear = rear;
+        }
+    }
+}
---
+++ Challenges/QueueWithStacks/QueueWithStacks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using StackAndQueue.Classes;
+
+namespace QueueWithStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void QueueWithStacks()
+        {
+
+        }
+    }
+}
