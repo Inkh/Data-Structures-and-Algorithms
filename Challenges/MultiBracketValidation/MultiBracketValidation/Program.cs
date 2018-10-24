d3371527ab943156061b35f980408b3db566ad5a
inkh <jhwe92@gmail.com>
2018-10-24 16:54:39 -0700

    Init. Added ref to Stack class

---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StackAndQueue.Classes;
+
+namespace MultiBracketValidation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
