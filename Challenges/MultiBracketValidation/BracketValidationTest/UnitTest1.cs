ac53cda2c335a6ab270d667e1bf6cb42af0732bf
inkh <jhwe92@gmail.com>
2018-10-25 11:22:42 -0700

    finished bracketvalidation method and added test files

---
+++ Challenges/MultiBracketValidation/BracketValidationTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using static MultiBracketValidation.Program;
+using StackAndQueue.Classes;
+
+namespace BracketValidationTest
+{
+    public class UnitTest1
+    {
+        Stack myStack = new Stack(null);
+
+        [Theory]
+        public void WillCheckBalanceTest(bool expected, string input)
+        {
+        
+        }
+    }
+}
