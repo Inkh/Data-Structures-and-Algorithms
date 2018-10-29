e217db8c9c002677fb769bb3f0f154467f6f17b2
inkh <jhwe92@gmail.com>
2018-10-28 18:56:17 -0700

    init for eeney

---
+++ Challenges/EeneyMeeneyMineyMoe/EeneyMeeneyMineyMoe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EeneyMeeneyMineyMoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
