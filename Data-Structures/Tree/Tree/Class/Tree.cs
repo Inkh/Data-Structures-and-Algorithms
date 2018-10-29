e683cd0c700656f75feb6af5da50e6f2b6d04c9f
inkh <jhwe92@gmail.com>
2018-10-29 10:07:49 -0700

    init. created node and tree class

---
+++ Data-Structures/Tree/Tree/Class/Tree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Class
+{
+    public class Tree
+    {
+        public Node Root { get; set; }
+
+        public Tree(Node root)
+        {
+            Root = root;
+        }
+    }
+}
