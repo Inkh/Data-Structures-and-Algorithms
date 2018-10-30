9e27eb9860c1448f80b0ed03a5871c14ceabcc99
inkh <jhwe92@gmail.com>
2018-10-29 17:28:06 -0700

    init

---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tree.Classes;
+
+namespace FizzBuzzTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
