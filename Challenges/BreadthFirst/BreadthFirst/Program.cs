5b88d8354b66c29e413ec13962684c198cf7930a
inkh <jhwe92@gmail.com>
2018-11-02 09:09:56 -0700

    constructed demo tree

--- Challenges/BreadthFirst/BreadthFirst/Program.cs
+++ Challenges/BreadthFirst/BreadthFirst/Program.cs
@@ -10,13 +10,38 @@ namespace BreadthFirst
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BinaryTree myTree = new BinaryTree(new Node(1));
+            myTree.Root.Left = new Node(2);
+            myTree.Root.Left.Left = new Node(4);
+            myTree.Root.Left.Right = new Node(5);
+
+            myTree.Root.Right = new Node(3);
+            myTree.Root.Right.Right = new Node(7);
+            myTree.Root.Right.Left = new Node(6);
+
+            //myTree.PreOrder();
+            BreadthFirstTraversal(myTree);
         }
 
         static void BreadthFirstTraversal(BinaryTree tree)
         {
             Queue<Node> myQ = new Queue<Node>();
+            myQ.Enqueue(tree.Root);
 
+            while (myQ.Peek() != null)
+            {
+                Node temp = myQ.Dequeue();
+                if (temp.Left != null)
+                {
+                    myQ.Enqueue(temp.Left);
+                }
+                else if (temp.Right != null)
+                {
+                    myQ.Enqueue(temp.Right);
+                }
 
+                Console.WriteLine(temp.Value);
+            }
         }
     }
 }
