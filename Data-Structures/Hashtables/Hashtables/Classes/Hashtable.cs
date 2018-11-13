7b288ec509fab822a815f46baf77c70758bd7267
inkh <jhwe92@gmail.com>
2018-11-13 13:58:05 -0800

    added custom node and SLL class

---
+++ Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using LinkedList.Classes;
+
+namespace Hashtables.Classes
+{
+    class Hashtable
+    {
+        public LList[] List { get; set; }
+
+        public Hashtable()
+        {
+            List = new LList[1024];
+        }
+    }
+}
---
+++ Data-Structures/Hashtables/Hashtables/Classes/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtables.Classes
+{
+    class Node
+    {
+        /// <summary>
+        /// Key contained within node. Can be different data types.
+        /// </summary>
+        public object Key { get; set; }
+
+        /// <summary>
+        /// Value contained within node. Can be different data types.
+        /// </summary>
+        public object Value { get; set; }
+
+        /// <summary>
+        /// The pointer going towards the next node in sequence.
+        /// </summary>
+        public Node Next { get; set; }
+
+        public Node(object key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
