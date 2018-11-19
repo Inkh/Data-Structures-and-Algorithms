34cabc8f260bbb94686e9afd0d353fb33f961f64
inkh <jhwe92@gmail.com>
2018-11-18 19:09:06 -0800

    init

---
+++ Challenges/MostCommonWord/MostCommonWord/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MostCommonWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] myArr = { 1, 1, 2, 1, 1 };
+        }
+
+        public static string MostCommon(string input)
+        {
+            string[] wordList = input.Split(" ");
+            string max = wordList[0];
+            
+
+            
+        }
+    }
+}
