ce1b88605eab924c9a78fe9ce9502041e7dad205
inkh <jhwe92@gmail.com>
2018-11-18 19:34:59 -0800

    method logic complete with type exceptions. pending fix

--- Challenges/MostCommonWord/MostCommonWord/Program.cs
+++ Challenges/MostCommonWord/MostCommonWord/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Hashtables.Classes;
 
 namespace MostCommonWord
 {
@@ -7,16 +8,36 @@ namespace MostCommonWord
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int[] myArr = { 1, 1, 2, 1, 1 };
+            string tempStr = "In a galaxy far far away";
+            Console.WriteLine(MostCommon(tempStr));
         }
 
-        public static string MostCommon(string input)
+        public static string MostCommon(string inputStr)
         {
-            string[] wordList = input.Split(" ");
+            string[] wordList = inputStr.Split(" ");
             string max = wordList[0];
-            
+            Hashtable table = new Hashtable();
+            int maxCount = 0;
 
-            
+            for(int i = 0; i < wordList.Length; i++)
+            {
+                if (table.Contains(wordList[i]))
+                {
+                    int temp = Convert.ToInt32(table.Find(wordList[i])) + 1;
+                    table.Add(wordList[i], temp);
+
+                    if (Convert.ToInt32(table.Find(wordList[i])) > maxCount)
+                    {
+                        maxCount = Convert.ToInt32(table.Find(wordList[i]));
+                        max = wordList[i];
+                    }
+                }
+                else
+                {
+                    table.Add(wordList[i], 1);
+                }
+            }
+            return max;       
         }
     }
 }
