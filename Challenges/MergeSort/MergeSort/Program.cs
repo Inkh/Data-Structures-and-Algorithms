96c51a68a09fb4832c9d5213359255eb50ee5793
inkh <jhwe92@gmail.com>
2018-11-21 19:16:42 -0800

    unfin

--- Challenges/MergeSort/MergeSort/Program.cs
+++ Challenges/MergeSort/MergeSort/Program.cs
@@ -8,5 +8,22 @@ namespace MergeSort
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void MergeSort(int[] arr)
+        {
+            if (arr.Length > 1)
+            {
+                int leftSize = arr.Length / 2;
+                int rightSize = arr.Length - leftSize;
+
+                //first half in left array
+                int[] left = new int[leftSize];
+                Array.Copy(arr, 0, left, 0, arr.Length / 2);
+
+                //second half in right array
+                int[] right = new int[rightSize];
+                Array.Copy(arr, arr.Length / 2, right, 0, arr.Length - left.Length);
+            }
+        }
     }
 }
