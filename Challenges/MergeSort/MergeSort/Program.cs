07c45c3099e593fbc889c06387ffab906dbe4955
inkh <jhwe92@gmail.com>
2018-11-26 11:15:59 -0800

    finished method

--- Challenges/MergeSort/MergeSort/Program.cs
+++ Challenges/MergeSort/MergeSort/Program.cs
@@ -9,6 +9,10 @@ namespace MergeSort
             Console.WriteLine("Hello World!");
         }
 
+        /// <summary>
+        /// Sorting algorithm that breaks array down into halves and eventually merges everything back together.
+        /// </summary>
+        /// <param name="arr">Array to be sorted</param>
         static void MergeSort(int[] arr)
         {
             if (arr.Length > 1)
@@ -23,7 +27,59 @@ namespace MergeSort
                 //second half in right array
                 int[] right = new int[rightSize];
                 Array.Copy(arr, arr.Length / 2, right, 0, arr.Length - left.Length);
+
+                MergeSort(left);
+                MergeSort(right);
+                Merge(left, right, arr);
             }
+        }
+
+        /// <summary>
+        /// Helper method that takes in three arrays. The left partition, right partition, and the base array.
+        /// </summary>
+        /// <param name="left">Left half</param>
+        /// <param name="right">Right half</param>
+        /// <param name="arr">Original</param>
+        /// <returns>Altered base array</returns>
+        static int[] Merge(int[] left, int[] right, int[] arr)
+        {
+            //left pointer
+            int i = 0;
+            //right pointer
+            int j = 0;
+            //end array pointer
+            int k = 0;
+
+            //makes sure that indexes are valid
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    //puts smaller value into main arr. in this case, left arr
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    //puts smaller value into main arr. in this case, right arr
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            if (i == left.Length)
+            {
+                //copy right arr into main arr
+                Array.Copy(right, j, arr, k, right.Length - j);
+            }
+            else
+            {
+                //copy left arr into main arr
+                Array.Copy(left, i, arr, k, left.Length - i);
+            }
+
+            return arr;
         }
     }
 }
