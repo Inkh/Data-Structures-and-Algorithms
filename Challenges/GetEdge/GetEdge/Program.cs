0bb6da9a2bfb9bc77f4e76c319b72854888be033
inkh <jhwe92@gmail.com>
2018-11-29 10:16:29 -0800

    init

---
+++ Challenges/GetEdge/GetEdge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Graph.Classes;
+
+namespace GetEdge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
